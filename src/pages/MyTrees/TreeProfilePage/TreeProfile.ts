e0637f3d2075bc5fd5b7f94a49c92123b1d1e10f
Yordan Vera <yordanveracastillo@gmail.com>
2017-01-01 19:44:09 -0300

    #2 se agrega la componente que maneja el perfil de un árbol.

--- src/pages/MyTrees/TreeProfilePage/TreeProfile.ts
+++ src/pages/MyTrees/TreeProfilePage/TreeProfile.ts
@@ -1,26 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { NavParams } from 'ionic-angular';
+import { Component }    from '@angular/core';
+import { NavParams }    from 'ionic-angular';
+import { TaskService }  from '../../../services/task.service';
+import { Task }         from '../../../models/task.model';
 
 @Component({
-    selector: 'TreeProfile',
-    templateUrl: 'TreeProfile.html'
+    selector    : 'TreeProfile',
+    templateUrl : 'TreeProfile.html',
+    providers   : [TaskService, Task]
 })
-export class TreeProfilePage implements OnInit {
+export class TreeProfilePage{
     private tree;
-    private isShowTask: boolean;
-    private isShowRecord: boolean;
-    constructor(params: NavParams) {
+    taskList: Task[] = [];
+    private option: string;
+
+    constructor(params: NavParams,
+                private taskService: TaskService) {
         this.tree = params.data.tree;
-        this.isShowTask = false;
-        this.isShowRecord = false;
+        this.option = "info";
+        this.taskService.getTasks()
+        .then(tasks => this.taskList = tasks);
      }
-
-    ngOnInit() { }
-
-    showTask() {
-        this.isShowTask = !this.isShowTask;
-    }
-    showRecord() {
-        this.isShowRecord = !this.isShowRecord;
-    }
 }
