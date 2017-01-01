824b5bae4e994c08f8e45e0ba4be6f9074a69efa
Yordan Vera <yordanveracastillo@gmail.com>
2017-01-01 19:38:32 -0300

    #2 actualización al servicio "tree.service"

--- src/services/tree.service.ts
+++ src/services/tree.service.ts
@@ -4,10 +4,41 @@ import { TREES } from './mock-trees';
 @Injectable()
 export class TreeService {
     protected list = TREES;
+    /**
+     * Obtiene la lista de árboles
+     * 
+     * @returns Promesa
+     * 
+     * @memberOf TreeService
+     */
     getTrees(){
         return Promise.resolve(this.list);
     }
+    /** 
+     * Agrega un árbol a la lista de árboles
+     * 
+     * @param {tree} any
+     * 
+     * @memberOf TreeService
+     */
     newTree(tree){
         this.list.push(tree);
+    }
+    /**
+     * Obtiene los datos de un árbol a partir de su identificador
+     * 
+     * @param {any} id_tree
+     * @returns 
+     * 
+     * @memberOf TreeService
+     */
+    getDataTree(id_tree){
+        return Promise.resolve();
+    }
+    deleteTree(){
+
+    }
+    updateTree(){
+
     }
 }
