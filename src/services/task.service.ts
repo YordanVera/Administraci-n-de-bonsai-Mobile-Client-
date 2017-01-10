034e073bb98e4194d612ab15eefb089c11399cb6
Yordan Vera <yordanveracastillo@gmail.com>
2017-01-10 14:31:31 -0300

    #5 se agrega al servicio de tareas la funcionalidad para agregar una nueva tarea a la lista.

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -3,17 +3,35 @@ import { TASKS } from './mock-tasks';
 
 @Injectable()
 export class TaskService {
+
     protected list = TASKS;
-    getTasks() {
+
+    getTasksByTree(id_tree: number) {
         return Promise.resolve(this.list);
     }
-    newTask(){
-        console.log('test');
+
+    newTask(task) {
+        var promise = new Promise((resolve) => {
+            if(this.list.length>0){
+                task.id_task = this.list[this.list.length-1].id_task + 1;
+            }else {
+                task.id_task = 1;
+            }
+            this.list.push(task);
+            resolve(task.id_task);
+        });
+        return promise;
     }
-    deleteTask(){
+
+    deleteTask() {
 
     }
-    updateTask(){
+
+    updateTask() {
         //upda
+    }
+
+    getTaskDetail(id_task: number) {
+        
     }
 }
