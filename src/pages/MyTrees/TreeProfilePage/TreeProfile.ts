995f7041262d72ec200631ab7f0b0241cfd37f62
Yordan Vera <yordanveracastillo@gmail.com>
2017-01-17 19:17:31 -0300

    #2 se agrega la opción para ejecutar el modal que permite agregar una tarea.

--- src/pages/MyTrees/TreeProfilePage/TreeProfile.ts
+++ src/pages/MyTrees/TreeProfilePage/TreeProfile.ts
@@ -1,7 +1,9 @@
-import { Component }    from '@angular/core';
-import { NavParams }    from 'ionic-angular';
-import { TaskService }  from '../../../services/task.service';
-import { Task }         from '../../../models/task.model';
+import { Component }                    from '@angular/core';
+import { NavParams, ModalController }   from 'ionic-angular';
+import { TaskService }                  from '../../../services/task.service';
+import { Task }                         from '../../../models/task.model';
+import { TaskDetailPage }               from '../../Task/TaskDetail/TaskDetail';
+import { NewTask }                      from '../../Task/NewTask/NewTask';
 
 @Component({
     selector    : 'TreeProfile',
@@ -9,15 +11,40 @@ import { Task }         from '../../../models/task.model';
     providers   : [TaskService, Task]
 })
 export class TreeProfilePage{
-    private tree;
+    public tree;
     taskList: Task[] = [];
-    private option: string;
+    public option: string;
 
     constructor(params: NavParams,
-                private taskService: TaskService) {
+                public taskService: TaskService,
+                public modalCtrl: ModalController) {
         this.tree = params.data.tree;
         this.option = "info";
-        this.taskService.getTasks()
+        this.taskService.getTasksByTree(this.tree.id_tree)
         .then(tasks => this.taskList = tasks);
+     }
+
+     openMoreModal(task) {
+         let modal = this.modalCtrl.create(TaskDetailPage, {task: task});
+         modal.present();
+     }
+
+     newTask() {
+         let modal = this.modalCtrl.create(NewTask);
+         modal.present();
+         modal.onDidDismiss(task => {
+             if(typeof task !== 'undefined'){
+                console.log(task);
+             }
+         });
+     }
+
+     deleteTask(task) {
+         let index = this.taskList.findIndex((tsk)=>{
+             return task.id_task === tsk.id_task;
+         });
+         if(index>-1){
+             this.taskList.splice(index,1);
+         }
      }
 }
